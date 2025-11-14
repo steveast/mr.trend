f0f9e6c1d3be8bae5112dee9449d5d1af95b446a
Steve <6086962+steveast@users.noreply.github.com>
2025-11-14 16:08:02 +0400

    up

--- restart.sh
+++ restart.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rm -r ./dist && pm2 stop mr-trend && git pull --rebase && npm run build && pm2 start mr-trend && pm2 log mr-trend --lines 50
+rm -r ./dist ; pm2 stop mr-trend && git pull --rebase && npm run build && pm2 start mr-trend && pm2 log mr-trend --lines 50
